b445ad755e8dbc713411a599cffbf462c0089686
James Andariese <james@strudelline.net>
2018-01-02 16:54:31 +0000

    add find_log_entry to find log entries by term and index and docs for it

--- README.md
+++ README.md
@@ -10,6 +10,43 @@ A [Raft][1] implementation in Elixir.
 GenServer.call(pid, {:requestVote, 0, 2, 0, 0})
 GenServer.call(pid, {:requestVote, 0, 2, 1, 0})
 GenServer.call(pid, {:requestVote, 0, 2, 0, 2})
+log = [
+    %{:index => 9, :term => 3, :value => 32},
+    %{:index => 8, :term => 3, :value => 31},
+    %{:index => 7, :term => 3, :value => 30},
+    %{:index => 6, :term => 3, :value => 29},
+    %{:index => 5, :term => 3, :value => 28},
+    %{:index => 4, :term => 3, :value => 27},
+    %{:index => 3, :term => 3, :value => 26},
+    %{:index => 2, :term => 3, :value => 25},
+    %{:index => 1, :term => 3, :value => 24},
+    %{:index => 13, :term => 2, :value => 23},
+    %{:index => 12, :term => 2, :value => 22},
+    %{:index => 11, :term => 2, :value => 21},
+    %{:index => 10, :term => 2, :value => 20},
+    %{:index => 9, :term => 2, :value => 19},
+    %{:index => 8, :term => 2, :value => 18},
+    %{:index => 7, :term => 2, :value => 17},
+    %{:index => 6, :term => 2, :value => 16},
+    %{:index => 5, :term => 2, :value => 15},
+    %{:index => 4, :term => 2, :value => 14},
+    %{:index => 3, :term => 2, :value => 13},
+    %{:index => 2, :term => 2, :value => 12},
+    %{:index => 1, :term => 2, :value => 11},
+    %{:index => 10, :term => 1, :value => 10},
+    %{:index => 9, :term => 1, :value => 9},
+    %{:index => 8, :term => 1, :value => 8},
+    %{:index => 7, :term => 1, :value => 7},
+    %{:index => 6, :term => 1, :value => 6},
+    %{:index => 5, :term => 1, :value => 5},
+    %{:index => 4, :term => 1, :value => 4},
+    %{:index => 3, :term => 1, :value => 3},
+    %{:index => 2, :term => 1, :value => 2},
+    %{:index => 1, :term => 1, :value => 1},
+]
+Skiff.find_log_entry(log, 4, 4)
+Skiff.find_log_entry(log, 3, 3)
+Skiff.find_log_entry(log, 0, 0)
 ```
 
 
--- lib/skiff.ex
+++ lib/skiff.ex
@@ -5,20 +5,18 @@ defmodule Skiff do
 
   use GenServer
 
-  def init(_args) do
-    Process.register(self(), :skiff)
-    {:ok, %{
-      :currentTerm => 0,
-      :votedFor => nil,
-      :log => [%{:term => 1, :index => 2}],
-      :commitIndex => 0,
-      :lastApplied => 0,
-      :nextIndex => %{},
-      :matchIndex => %{},
-    }}
+  defmodule State do
+    defstruct currentTerm: 0, votedFor: nil, log: [%{:term => 1, :index => 2}], commitIndex: 0, lastApplied: 0, nextIndex: %{}, matchIndex: %{}
   end
 
-  
+  def init(_args) do
+    Process.register(self(), :skiff)
+    {:ok, %State{}}
+  end
+
+  ########################
+  # index_and_term_gte_log
+
   # test if an index, term pair are >= a log's last entry
   # first, handle the easy cases
   defp index_and_term_gte_log(_index, term, [%{:term => log_term} | _rest]) when term > log_term, do: true
@@ -28,35 +26,54 @@ defmodule Skiff do
   # finally, we've verified the log isn't *more* up to date (including when there's no log at all)
   defp index_and_term_gte_log(_index, _term, _), do: true
 
+  ########################
+  # RequestVote
+
   @doc """
     Reject a vote request when the candidate's term is less than our current term.
   """
   def handle_call({:requestVote, term, _candidateId, _lastLogIndex, _lastLogTerm}, _from, state = %{:currentTerm => ct}) when term < ct do
-    {:reply, {state[:currentTerm], false}, state}
+    {:reply, {state.currentTerm, false}, state}
   end
 
   @doc """
-    ...or accept a vote request when we've note voted yet and that candidate's log is at least as up to date as ours
-  """
-  def handle_call({:requestVote, _term, candidateId, lastLogIndex, lastLogTerm}, _from, state = %{:votedFor => nil}) do
-    if index_and_term_gte_log(lastLogIndex, lastLogTerm, state[:log]) do
-      {:reply, {state[:currentTerm], true}, Map.put(state, :votedFor, candidateId)}
+    ...or accept a vote request when we've note voted yet or voted for this candidate and that candidate's log is at least as up to date as ours
+    if we've gotten an update to our log since this candidate started their campaign tour, we will withdraw our vote
+    """
+    def handle_call({:requestVote, _term, candidateId, lastLogIndex, lastLogTerm}, _from, state = %{:votedFor => vf}) when vf == nil or vf == candidateId do
+    # doing this `if` here instead of inside the guard only works because the fallthrough is what comes next.  if another test were to come after this,
+    # it would break because the `if` here would short circuit any future tests.  this bit of messiness is because I don't know elixir yet.  sorry.
+    if index_and_term_gte_log(lastLogIndex, lastLogTerm, state.log) do
+      {:reply, {state.currentTerm, true}, %{state | votedFor: candidateId}}
     else
-      {:reply, {state[:currentTerm], false}, state}
+      {:reply, {state.currentTerm, false}, state}
     end
   end
-
-  @doc """
-    ...or accept a vote if the candidate requesting the vote is still our chosen candidate
-  """
-  def handle_call({:requestVote, _term, candidateId, _lastLogIndex, _lastLogTerm}, _from, state = %{:votedFor => vf}) when vf == candidateId do
-    {:reply, {state[:currentTerm], true}, state}
-  end
-
+  ############################################################################
+  # if you're adding something here, check out the comment about 8 lines up. #
+  ############################################################################
   @doc """
      ...otherwise, reject the vote request.
   """
   def handle_call({:requestVote, _term, _candidateId, _lastLogIndex, _lastLogTerm}, _from, state) do
-    {:reply, {state[:currentTerm], false}, state}
+    {:reply, {state.currentTerm, false}, state}
+  end
+
+  # find the log entry
+  def find_log_entry([], _term, _index), do: nil
+  def find_log_entry([log = %{:term => log_term, :index => log_index} | _rest], term, index) when term == log_term and index == log_index, do: log
+  def find_log_entry([%{:term => log_term, :index => log_index} | _rest], term, index) when term == log_term and index > log_index, do: nil
+  def find_log_entry([%{:term => log_term} | _rest], term, _index) when term > log_term, do: nil
+  def find_log_entry([_head | rest], term, index), do: find_log_entry(rest, term, index)
+
+  ########################
+  # AppendEntries
+  @doc """
+  """
+  def handle_call({:appendEntries, term, leaderId, prevLogIndex, prevLogTerm, entries, leaderCommit}, _from, state) do
+    if term < state.currentTerm do
+      false
+    else
+    end
   end
 end
